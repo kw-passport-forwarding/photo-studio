9a8b00c7e2314394e4bc36f4153aff01df51fb92
FKgk <rhkd865@gmail.com>
2023-10-25 22:07:24 +0900

    fix: AWS upload 기능 추가

--- app/image_storage.py
+++ app/image_storage.py
@@ -2,24 +2,28 @@ from datetime import datetime
 
 import boto3
 
-from settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_BUCKET
+from settings import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_BUCKET, AWS_REGION_NAME
 
 
 class Boto3Client:
     def __init__(self):
         self.client = boto3.client(
-            's3',
+            service_name='s3',
+            region_name=AWS_REGION_NAME,
             aws_access_key_id=AWS_ACCESS_KEY_ID,
             aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         )
 
     def upload(self, file, file_name):
-        extension = file_name.split('.')[-1]
+        save_file_name = f'{datetime.now().timestamp()}-{file_name}'
+
         self.client.upload_fileobj(
             file,
             AWS_DEFAULT_BUCKET,
-            f'input/{file_name}-{datetime.now().isoformat()}.{extension}'
+            f'input/{save_file_name}'
         )
+
+        return save_file_name
 
 
 boto3Client = Boto3Client()
--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 import requests
 import streamlit as st
 
+from image_storage import boto3Client
+
 # page setting
 st.set_page_config(
     page_title='passport forwarding',
@@ -21,6 +23,7 @@ if img_file is not None:
     with input_content:
         st.subheader('Your photo')
         st.image(img_file, width=512)
+        boto3Client.upload(img_file, img_file.name)
 
     response_image = requests.get('https://images.khan.co.kr/article/2022/06/09/l_2022060902000459200088941.jpg')
 
